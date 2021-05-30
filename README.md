d548546447cb52da5522729f96e2b1fe9a3e4cd7
Brooks J Rady <b.j.rady@gmail.com>
2021-05-30 10:56:18 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Developing
+
+```bash
+# First-time setup
+python -m venv .venv
+
+# Activate (for fish)
+source .venv/bin/activate.fish
+
+# Upgrade pip and install packages
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Deactivate virtual environment
+deactivate
+```
+
+# Things to Parse
+
+```
+Na+ GM-AEJA-GM-AEJ (x2) (Anhydro) |2
+Na+ GM-AEJA-GM-AEJ (Anhydro) |2
+Na+ GM-AEJA-GM-AEJ|2
+GM-AEJA-GM-AEJ|2
+```
+
+# Questions
+
+- What are the "mods" in the `masses.txt` file? Are they the numbers after the inferred structures in MS1?
---
+++ loading.py
@@ -0,0 +1,24 @@
+import pandas
+import re
+
+class Structure:
+    pattern = re.compile(r'(?:(\S*) )?([A-Z-]+)(?: \(x(\d)\))?(?: \((.*)\) )?\|(\d)')
+    def __init__(self, structure):
+        ion, struct, mult, annot, mod = self.pattern.match(structure).groups()
+        # FIXME: I should eventually do some validation work here!
+        self.ion = ion
+        self.structure = struct
+        self.multiplier = int(mult) if mult else None
+        self.annotation = annot
+        self.modification = int(mod) if mod else None
+
+    def __repr__(self):
+        return str(vars(self))
+
+def parse_structures(structures):
+    return list(map(Structure, structures.split(',')))
+
+# Read all non-NA `inferredStructures` from the MS1 data
+ms1_structures = pandas.read_csv('data/ms1.csv')['inferredStructure'].dropna()
+# Process the `inferredStructures` into a more amiable format
+ms1_structures =  ms1_structures.apply(parse_structures)
