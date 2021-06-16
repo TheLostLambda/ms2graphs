ff4d0c847fa32682a1a4cd8b16fffcb18c55749b
Brooks J Rady <b.j.rady@gmail.com>
2021-06-16 18:42:52 +0100

    Fixed some pylint warnings

--- loading.py
+++ loading.py
@@ -1,8 +1,11 @@
 import pandas
 import re
 
+
 class Structure:
-    pattern = re.compile(r'(?:(\S*) )?([A-Z-]+)(?: \(x(\d)\))?(?: \((.*)\) )?\|(\d)')
+    regex = r'(?:(\S*) )?([A-Z-]+)(?: \(x(\d)\))?(?: \((.*)\) )?\|(\d)'
+    pattern = re.compile(regex)
+
     def __init__(self, structure):
         ion, struct, mult, annot, mod = self.pattern.match(structure).groups()
         # FIXME: I should eventually do some validation work here!
@@ -15,10 +18,13 @@ class Structure:
     def __repr__(self):
         return str(vars(self))
 
+
 def parse_structures(structures):
     return list(map(Structure, structures.split(',')))
+
 
 # Read all non-NA `inferredStructures` from the MS1 data
 ms1_structures = pandas.read_csv('data/ms1.csv')['inferredStructure'].dropna()
 # Process the `inferredStructures` into a more amiable format
-ms1_structures =  ms1_structures.apply(parse_structures)
+ms1_structures = ms1_structures.apply(parse_structures)
+
